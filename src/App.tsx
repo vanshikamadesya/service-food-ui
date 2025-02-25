dff98977dae9aff1dd30dc0122e3f3dfc358c5d3
Vanshika <madesyavanshika@gmail.com>
2025-02-25 23:36:24 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import './index.css'
-
+import Footer from './components/footer'
 function App() {
 
   return (
     <>
-    <div></div>
+    <div>
+      <Footer/>
+    </div>
     </>
   )
 }
