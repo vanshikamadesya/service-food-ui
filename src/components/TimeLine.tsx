3e8d7f7e4524f581528ec3cc51c4bbd3f4a8a396
16D06 <121073997+16D06@users.noreply.github.com>
2025-02-27 12:49:15 +0530

    add ui

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -18,8 +18,8 @@ const Timeline: React.FC<TimelineProps> = ({ items }) => {
   };
 
   return (
-    <div className="bg-[#EFEBE7] p-3 relative w-full mx-auto">
-      <div className="flex justify-center mb-10">
+    <div className="bg-[#EFEBE7]  px-4 md:px-8 py-10 relative w-full  mx-auto">
+      <div className="flex justify-center mb-6">
         <h1 className="text-[#3A342B] text-3xl font-bold text-center mb-6 pb-3 border-b-4 border-[#3A342B]">
           Our History
         </h1>
@@ -27,7 +27,7 @@ const Timeline: React.FC<TimelineProps> = ({ items }) => {
 
       {/* Timeline with years */}
       <div className="relative flex items-center justify-between w-full">
-        <div className="absolute top-[50%] left-0 w-full h-0.5 bg-gray-300 translate-y-3" />
+        <div className="absolute top-[55%] left-0 w-full h-0.5 bg-gray-300 translate-y-3" />
 
         {items.map((item, index) => (
           <div
@@ -78,7 +78,7 @@ const Timeline: React.FC<TimelineProps> = ({ items }) => {
           className="w-full"
           spaceBetween={50}
           centeredSlides={true}
-          slidesPerView={1.4}
+          slidesPerView={1.5}
         >
           {items.map((item, index) => (
             <SwiperSlide
@@ -90,22 +90,24 @@ const Timeline: React.FC<TimelineProps> = ({ items }) => {
                   : "opacity-50 bg-[#EFEBE733]"
               )}
             >
-              <div className="flex flex-col md:flex-row items-stretch p-6 max-w-5xl mx-auto">
+              <div className="flex flex-col lg:flex-row items-stretch p-6 max-w-5xl mx-auto">
                 {/* Image Section */}
                 <div className="w-full md:w-1/2 h-full">
                   <img
                     src={item.image}
-                    className="w-full h-full object-cover rounded-lg"
+                    className="w-full h-full object-cover "
                   />
                 </div>
 
                 {/* Text Content Section */}
-                <div className="w-full md:w-1/2 bg-white p-6 shadow-lg rounded-lg flex flex-col justify-center">
-                  <h2 className="text-[#86704D] text-4xl font-bold">
+                <div className="w-full md:w-1/2 bg-white p-6 shadow-lg  flex flex-col justify-center">
+                  <h2 className="text-[#86704D] text-2xl md:text-3xl lg:text-4xl font-bold">
                     {item.year}
                   </h2>
 
-                  <p className="text-[#86704D] mt-4">{item.description}</p>
+                  <p className="text-[#86704D] mt-4 text-sm md:text-base">
+                    {item.description}
+                  </p>
                 </div>
               </div>
             </SwiperSlide>
@@ -115,18 +117,18 @@ const Timeline: React.FC<TimelineProps> = ({ items }) => {
 
       {/* Navigation Buttons */}
 
-      <div className="flex justify-end gap-6 mt-6 right-6">
+      <div className="flex justify-end gap-6 mt-3 right-7">
         <button
           onClick={() => swiperInstance?.slidePrev()}
           className="text-[#3A342B] p-3 bg-transparent border-none cursor-pointer"
         >
-          <HiArrowNarrowLeft size={40} />
+          <HiArrowNarrowLeft className="w-8 h-8 md:w-10 md:h-10" />
         </button>
         <button
           onClick={() => swiperInstance?.slideNext()}
           className="text-[#3A342B] p-3 bg-transparent border-none cursor-pointer"
         >
-          <HiArrowNarrowRight size={40} />
+          <HiArrowNarrowRight className="w-8 h-8 md:w-10 md:h-10" />
         </button>
       </div>
     </div>
