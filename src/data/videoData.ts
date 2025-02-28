30747c561a7d3b08ae87ef6f39cb90c5df046a21
TusharSavaliya987 <tusharsavaliya987@gmail.com>
2025-02-28 12:19:04 +0530

    fix props

---
+++ src/data/videoData.ts
@@ -0,0 +1,10 @@
+import Video from "../assets/images/more-video.mp4";
+
+export const videoData = [
+  { videoSrc: Video, title: "LOREM IPSUM 1", description: "Lorem Ipsum is simply dummy text of the printing" },
+  { videoSrc: Video, title: "LOREM IPSUM 2", description: "Lorem Ipsum is simply dummy text of the printing" },
+  { videoSrc: Video, title: "LOREM IPSUM 3", description: "Lorem Ipsum is simply dummy text of the printing" },
+  { videoSrc: Video, title: "LOREM IPSUM 4", description: "Lorem Ipsum is simply dummy text of the printing" },
+  { videoSrc: Video, title: "LOREM IPSUM 5", description: "Lorem Ipsum is simply dummy text of the printing" },
+  { videoSrc: Video, title: "LOREM IPSUM 6", description: "Lorem Ipsum is simply dummy text of the printing" },
+];
