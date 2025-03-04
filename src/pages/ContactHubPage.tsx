b657292ec661f2654efffc657f1809e65f5f64c4
Vanshika <madesyavanshika@gmail.com>
2025-03-04 10:12:36 +0530

    modified

--- src/pages/ContactHubPage.tsx
+++ src/pages/ContactHubPage.tsx
@@ -1,10 +1,14 @@
 import ContentHub from "../components/ContactHub"
 import OurStory from "../components/OurStories"
+// import OurVideos from "../components/OurVideo"
+import VideoSection from "../components/VideoSection"
 const ContactHubPage = () => {
   return (
     <div>
       <ContentHub/>
       <OurStory/>
+      {/* <OurVideos/> */}
+      <VideoSection/>
     </div>
   )
 }
