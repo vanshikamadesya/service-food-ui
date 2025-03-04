4b740c37053f4c5dc9a90afa003cef7dbd6a22b0
Vanshika <madesyavanshika@gmail.com>
2025-03-04 16:26:12 +0530

    updated code

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import OncePlace from "../components/once-place";
 import OurCategories from "../components/our-categories";
 import ExclusiveBrands from "../components/exclusive-brands";
